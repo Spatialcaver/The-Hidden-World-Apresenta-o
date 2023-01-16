cbf0274f624c4895c2094322e8a554e40910517b
Spatialcaver <101186168+Spatialcaver@users.noreply.github.com>
2023-01-16 13:34:20 -0300

    Add files via upload

---
+++ validador.js
@@ -0,0 +1,12 @@
+var  nome = document.getElementById("nome").value
+var telefone= document.getElementById("telefone").value
+var email= document.getElementById("email").value
+var personagem=document.getElementById("personagem").value
+
+
+function informar(){
+    
+alert("Agradecemos seu interesse em fazer parte de nossa comunidade, entraremos em contato logo logo!")
+
+}
+    
